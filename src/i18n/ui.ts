b672483e12e7c43a20cf51d2c64a9b46abb1c308
Agustín Barcia <agustinbarcia.dev@gmail.com>
2024-11-01 12:09:33 -0300

    language switcher buttons

---
+++ src/i18n/ui.ts
@@ -0,0 +1,18 @@
+export const languages = {
+  en: 'EN',
+  es: 'ES',
+};
+
+export const defaultLang = 'en';
+
+export const ui = {
+  en: {
+    'nav.home': 'Home',
+    'nav.about': 'About',
+    'nav.twitter': 'Twitter',
+  },
+  es: {
+    'nav.home': 'Home',
+    'nav.about': 'Sobre Mí',
+  },
+} as const;
